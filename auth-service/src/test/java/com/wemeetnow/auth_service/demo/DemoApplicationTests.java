5a9eb0b837e5cbbbbc44b797b02b81185ec976f5
kyunghwan1207 <kyunghwanko1207@gmail.com>
2024-09-03 21:33:47 +0900

    init: auth-service

---
+++ auth-service/src/test/java/com/wemeetnow/auth_service/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemeetnow.auth_service.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

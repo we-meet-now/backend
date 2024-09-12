b7d1098814a59da357c10666ea79ee242c0a838e
kyunghwan1207 <kyunghwanko1207@gmail.com>
2024-09-12 20:38:47 +0900

    🍀[FEAT] auth-service 생성

--- auth-service/src/main/java/com/wemeetnow/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/wemeetnow/auth_service/AuthServiceApplication.java
@@ -1,13 +1,13 @@
-package com.wemeetnow.auth_service.demo;
+package com.wemeetnow.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
--- auth-service/src/test/java/com/wemeetnow/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/wemeetnow/auth_service/AuthServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.wemeetnow.auth_service.demo;
+package com.wemeetnow.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
